f6aacdc8925879045d90babc459aaed7a310a72a
Aniketh <anikethnair97@gmail.com>
2019-12-06 20:07:55 +0530

    Skills Layout and Styles

--- src/pages/Home/Skills.js
+++ src/pages/Home/Skills.js
@@ -2,8 +2,12 @@ import React from 'react';
 
 export default function Skills(){
     return(
-        <div>
-            <p>Skills Skills Skills Skills Skills Skills Skills Skills Skills Skills Skills Skills Skills Skills Skills Skills Skills Skills Skills Skills Skills Skills Skills Skills Skills Skills Skills Skills Skills Skills Skills Skills Skills Skills Skills Skills Skills Skills Skills Skills Skills Skills Skills Skills </p>
+        <div className="skills">
+            <h2 className="heading">"Skills"</h2>
+            <p><b>Technologies:</b> HTML5, CSS3, SCSS, JavaScript, ES6, PHP, Python</p>
+            <p>Frameworks: <b>React, Redux, jQuery, BulmaJS, Bootstrap, Flask </b></p>
+            <p><b>Tools:</b> Adobe Suite (XD, Illustrator, Photoshop), Visual Studio Code, Sublime Text, Git</p>
+            <p>Database: <b>MySQL, MongoDB</b></p>
         </div>
     )
 }
