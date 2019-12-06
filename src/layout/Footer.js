f13173aa6026b2db04bfd6873cdc771bd59a9641
Aniketh <anikethnair97@gmail.com>
2019-12-06 20:20:17 +0530

    Footer | Home Page Complete!

--- src/layout/Footer.js
+++ src/layout/Footer.js
@@ -1,9 +1,12 @@
 import React from 'react';
 
+import react from 'static/images/layout/react.svg';
+import love from 'static/images/layout/love.svg';
+
 export default function Footer() {
     return (
         <div className="footer">
-            Footer
+            <p>Built with <img src={react} alt="React" /> &amp; <img src={love} alt="Heart" /> by <b>Aniketh Nair</b></p>
         </div>
     )
 }
