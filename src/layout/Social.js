b2dfdd346a43ed78a39bece86199497abc7e64a3
Aniketh <anikethnair97@gmail.com>
2019-12-10 12:18:20 +0530

    Bug fixes

--- src/layout/Social.js
+++ src/layout/Social.js
@@ -38,14 +38,20 @@ export default function Social() {
             if (ref.current && !ref.current.contains(event.target)) {
                 setState(false);
             }
+            else{
+                setState(true)
+            }
         }
 
         useEffect(() => {
             // Bind the event listener
             document.addEventListener("mousedown", handleClickOutside);
+            document.addEventListener("touchstart", handleClickOutside);
+
             return () => {
                 // Unbind the event listener on clean up
                 document.removeEventListener("mousedown", handleClickOutside);
+                document.removeEventListener("touchstart", handleClickOutside);
             };
         });
     }
